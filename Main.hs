f79050c4ef9a0bd9273c2961afbbe54fc840bc06
shein <andrey.shein.spb@gmail.com>
2020-11-06 17:33:56 +0300

    upgrade productivity

--- Main.hs
+++ Main.hs
@@ -1,5 +1,4 @@
-
-import Data.Maybe
+import Data.Set ( Set, singleton, union, member, delete )
 
 
 type Symb = String 
@@ -14,69 +13,68 @@ data Expr = Var Symb
           deriving (Eq, Read, Show)
 
 
-freeVars :: Expr -> [Symb]
-freeVars (Var symb)        = [symb]
-freeVars (first :@ second) = (freeVars first) ++ (freeVars second)
-freeVars (Lam symb expr)   = filter (/= symb) (freeVars expr)
+freeVars :: Expr -> Set Symb
+freeVars (Var s)   = singleton s
+freeVars (l :@ r)  = (freeVars l) `union` (freeVars r)
+freeVars (Lam s e) = delete s (freeVars e)
 
 
-make_unique :: Symb -> [Symb] -> Symb
-make_unique name list = if   elem name list 
-                        then make_unique (name ++ "'") list
-                        else name 
+make_unique :: Symb -> Set Symb -> Set Symb -> Symb
+make_unique n f s = if   member n f || member n s 
+                    then make_unique ('\'' : n) f s
+                    else n
 
 
 -- term[var := subterm] --
 subst :: Symb -> Expr -> Expr -> Expr 
-subst var subterm (Var name)        = if name == var then subterm else (Var name)
-subst var subterm (first :@ second) = (subst var subterm first) :@ (subst var subterm second)
-subst var subterm (Lam symb expr)   = if symb == var then (Lam symb expr) else result            -- TODO --
-                                          where free_subterm = freeVars subterm
-                                                free_expr    = freeVars expr
-                                                result       = if   elem symb free_subterm
-                                                               then let name   = make_unique symb (free_subterm ++ free_expr)
-                                                                        first  = subst symb (Var name) expr
-                                                                        second = subst var subterm first 
-                                                                    in  Lam name second 
-                                                               else Lam symb (subst var subterm expr)
+subst v n buf@(Var s)   | s == v    = n
+                        | otherwise = buf
+
+subst v n (l :@ r)      = (subst v n l) :@ (subst v n r)
+
+subst v n buf@(Lam s e) | s == v    = buf
+                        | otherwise = if   member s (freeVars n)
+                                      then let name = make_unique s (freeVars n) (freeVars e)
+                                           in  Lam name $ subst v n $ subst s (Var name) e
+                                      else Lam s (subst v n e)
 
 
 -- alpha equivalence --
 alphaEq :: Expr -> Expr -> Bool
-alphaEq (Var first)       (Var second)      = (first == second)
-alphaEq (left1 :@ right1) (left2 :@ right2) = (left1 `alphaEq` left2) && (right1 `alphaEq` right2)
-alphaEq (Lam symb1 expr1) (Lam symb2 expr2) = let first  = expr1 `alphaEq` (subst symb2 (Var symb1) expr2)
-                                                  second = expr2 `alphaEq` (subst symb1 (Var symb2) expr1)
-                                              in  first && second
-alphaEq _                 _                 = False
+alphaEq (Var f)     (Var s)     = (f == s)
+
+alphaEq (l1 :@ r1)  (l2 :@ r2)  = (l1 `alphaEq` l2) && (r1 `alphaEq` r2)
+
+alphaEq (Lam s1 e1) (Lam s2 e2) | s1 == s2  = e1 `alphaEq` e2
+                                | otherwise = if   member s1 $ freeVars e2 
+                                              then False
+                                              else e1 `alphaEq` (subst s2 (Var s1) e2)
+
+alphaEq _           _           = False
                                             
 
 -- beta reduction --
 reduceOnce :: Expr -> Maybe Expr
-reduceOnce (Var _)                    = Nothing
+reduceOnce (Var _)          = Nothing
 
-reduceOnce ((Lam symb expr) :@ right) = Just (subst symb right expr)
+reduceOnce ((Lam s e) :@ r) = Just (subst s r e)
 
-reduceOnce (left :@ right)            = let left'   = reduceOnce left 
-                                            right'  = reduceOnce right
-                                        in  if   left' == Nothing && right' == Nothing
-                                            then Nothing
-                                            else let left''  = maybe left id left'
-                                                     right'' = maybe right id right'
-                                                 in  Just (left'' :@ right'')
+reduceOnce (l :@ r)         = case reduceOnce l of 
+                                  Nothing -> case reduceOnce r of
+                                                 Nothing -> Nothing
+                                                 Just r' -> Just (l :@ r')
+                                  Just l' -> Just (l' :@ r) 
 
-reduceOnce (Lam symb expr)            = let result = reduceOnce expr 
-                                        in  if   result == Nothing 
-                                            then Nothing
-                                            else Just (Lam symb $ fromJust result)
+reduceOnce (Lam symb expr)  = case reduceOnce expr of 
+                                  Nothing   -> Nothing
+                                  Just elem -> Just (Lam symb elem)
 
 
 -- beta reduction using normal strategy
 nf :: Expr -> Expr 
-nf expr = let reduce = reduceOnce expr
-          in  if reduce == Nothing
-              then expr
-              else nf $ fromJust reduce 
+nf expr = case reduceOnce expr of 
+              Nothing   -> expr
+              Just elem -> nf elem
 
 
 -- beta equivalence
