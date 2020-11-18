0db1e0f0110e3a5487b4531101c9e26c47054371
shein <andrey.shein.spb@gmail.com>
2020-11-18 17:08:00 +0300

    more readable

--- Main.hs
+++ Main.hs
@@ -15,14 +15,14 @@ data Expr = Var Symb
 
 freeVars :: Expr -> Set Symb
 freeVars (Var s)   = singleton s
-freeVars (l :@ r)  = (freeVars l) `union` (freeVars r)
+freeVars (l :@ r)  = freeVars l `union` freeVars r
 freeVars (Lam s e) = delete s (freeVars e)
 
 
-make_unique :: Symb -> Set Symb -> Set Symb -> Symb
-make_unique n f s = if   member n f || member n s 
-                    then make_unique ('\'' : n) f s
-                    else n
+makeUnique :: Symb -> Set Symb -> Set Symb -> Symb
+makeUnique n f s = if   member n f || member n s 
+                   then makeUnique ('\'' : n) f s
+                   else n
 
 
 -- term[var := subterm] --
@@ -30,25 +30,24 @@ subst :: Symb -> Expr -> Expr -> Expr
 subst v n buf@(Var s)   | s == v    = n
                         | otherwise = buf
 
-subst v n (l :@ r)      = (subst v n l) :@ (subst v n r)
+subst v n (l :@ r)      = subst v n l :@ subst v n r
 
 subst v n buf@(Lam s e) | s == v    = buf
                         | otherwise = if   member s (freeVars n)
-                                      then let name = make_unique s (freeVars n) (freeVars e)
+                                      then let name = makeUnique s (freeVars n) (freeVars e)
                                            in  Lam name $ subst v n $ subst s (Var name) e
                                       else Lam s (subst v n e)
 
 
 -- alpha equivalence --
 alphaEq :: Expr -> Expr -> Bool
-alphaEq (Var f)     (Var s)     = (f == s)
+alphaEq (Var f)     (Var s)     = f == s
 
 alphaEq (l1 :@ r1)  (l2 :@ r2)  = (l1 `alphaEq` l2) && (r1 `alphaEq` r2)
 
 alphaEq (Lam s1 e1) (Lam s2 e2) | s1 == s2  = e1 `alphaEq` e2
-                                | otherwise = if   member s1 $ freeVars e2 
-                                              then False
-                                              else e1 `alphaEq` (subst s2 (Var s1) e2)
+                                | otherwise = not (member s1 $ freeVars e2)
+                                              && (e1 `alphaEq` subst s2 (Var s1) e2)
 
 alphaEq _           _           = False
                                             
@@ -72,11 +71,9 @@ reduceOnce (Lam symb expr)  = case reduceOnce expr of
 
 -- beta reduction using normal strategy
 nf :: Expr -> Expr 
-nf expr = case reduceOnce expr of 
-              Nothing   -> expr
-              Just elem -> nf elem
+nf expr = maybe expr nf (reduceOnce expr)
 
 
 -- beta equivalence
 betaEq :: Expr -> Expr -> Bool 
-betaEq first second = (nf first) `alphaEq` (nf second)
+betaEq first second = nf first `alphaEq` nf second
